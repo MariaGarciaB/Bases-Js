21f75118ee79a400936e09d05717acef7410ad00
Maria Garcia <licgarcia_psic@hotmail.com>
2023-09-18 22:38:08 -0600

    variables

--- js0-variables.js
+++ js0-variables.js
@@ -11,15 +11,25 @@ let linkedinProfile = "/mariagarbar";
 let favoriteBand = "Morat";
 
 //2. Crea 10 variables de tipo number (numero) y asígnale cualquier número.
+let date = 20230918;
 let year = 2023;
 let month = 9;
 let day = 18;
 let decimals = 12.345;
 let pi = 3.1416;
-let mes = 9;
-let mes = 9;
-let mes = 9;
-let mes = 9;
-let mes = 9;
+let time = 9;
+let hour = 22;
+let minutes = 23;
+let seconds = 9;
 
 // 3. Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.
+let areUaWoman = true;
+let ivaIncluded = false;
+let customer = true;
+let registered = true;
+let usedPython = false;
+let seniorDeveloper = false;
+let usedFirebase = true;
+let latinAmerican = true;
+let usedNodeJs = true;
+let designer = false;
