9080d5dc52b8e20ef7f5d95638d10e6a6f49ce6b
Maria Garcia <licgarcia_psic@hotmail.com>
2023-09-18 22:22:19 -0600

    ejercicios variables

---
+++ js0-variables.js
@@ -0,0 +1,25 @@
+//1. Crea 10 variables de tipo string (cadena de texto) y asignale cualquier texto. 
+let sayHi = "Hi world";
+let name = "Maria García";
+let age = "unknown :)";
+let oldProfession = "Psicóloga";
+let newProfession = "Developer en construcción";
+let bestCountry = "México <3";
+let programmingLanguage = "JavaScript";
+let gitHubProfile = "/MariaGarciaB"
+let linkedinProfile = "/mariagarbar";
+let favoriteBand = "Morat";
+
+//2. Crea 10 variables de tipo number (numero) y asígnale cualquier número.
+let year = 2023;
+let month = 9;
+let day = 18;
+let decimals = 12.345;
+let pi = 3.1416;
+let mes = 9;
+let mes = 9;
+let mes = 9;
+let mes = 9;
+let mes = 9;
+
+// 3. Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.
